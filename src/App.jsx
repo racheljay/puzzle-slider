c4a975c714372bb9918b2247f0f8b1d647837f01
Rachel <racheljay42@gmail.com>
2020-10-29 10:08:00 -0400

    fixed index/ id logic error, basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ class App extends React.Component {
 
     // this.blank = gameTiles[0]
     this.state = {
-      gameTiles: []
+      gameTiles: [],
+      blank: 0
     }
 
     this.renderWins = this.renderWins.bind(this);
@@ -26,19 +27,24 @@ class App extends React.Component {
       gameTiles: arr
     })
 
-    console.log(this.state.gameTiles)
+    console.log(this.state.gameTiles, this.state.blank)
 
   }
 
   updateTileStatus() {
-    let blank = this.state.gameTiles.findIndex((element) => element.id === 0)
+    console.log('working');
+    console.log(this.state.gameTiles)
+    let blank = this.state.blank
+    // this.state.gameTiles.findIndex((element) => element.id === 0)
+
+
     let newArr = this.state.gameTiles.map((item, index) => {
 
-      // if (item.id === 0) {
-      //   item.status = false
+      if (item.id === 0) {
+        item.status = false
 
-      // }
-      if (index === blank + 1 || index === blank - 1 || index === blank + 4 || index === blank - 4) {
+      }
+      else if (index === blank + 1 || index === blank - 1 || index === blank + 4 || index === blank - 4) {
         item.status = true
       }
       else {
@@ -46,7 +52,8 @@ class App extends React.Component {
       }
       return item
     })
-    //if check if new array is different from game tiles
+    //check if new array is different from game tiles
+
     this.setState({
       gameTiles: newArr
     })
@@ -57,19 +64,60 @@ class App extends React.Component {
     this.renderWins()
 
 
+
   }
 
-  componentDidUpdate() {
-    this.updateTileStatus()
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.blank !== prevState.blank) {
+      this.updateTileStatus()
+   }
   }
 
+  // clickable(num) {
+  //   console.log(num.status)
+  // }
+  initBoard(){
+      console.log("i wrk")
+      let arr = this.state.gameTiles.map((item, index) => {
+        if(item.id === 1 || item.id === 4) {
+          item.status = true
+        }
+       
+       return item
+      })
 
-  clickAction(num) {
-    this.state.gameTiles.map((item, index) => {
-      if (item.status === true) {
-        console.log(item)
-      }
-    })
+      console.log(arr)
+      this.setState({
+      gameTiles: arr
+      })
+
+  }
+
+  clickAction(item, index) {
+    if (item.status === true) {
+      
+      let arr = this.state.gameTiles;
+      let blank = this.state.blank;
+    
+      [arr[index], arr[blank]] = [arr[blank], arr[index]];
+    
+      this.setState({ gameTiles: arr, blank: index })
+      console.log("in if statement setting state", blank, index)
+    } console.log(item)
+    
+    
+    // this.state.gameTiles.map((item, index) => {
+
+    // let arr = this.state.gameTiles
+    // let target = num
+    // let blank = this.state.blank
+    // [arr[blank], arr[target]] = [arr[target], arr[blank]]
+
+    // arr.num = arr.blank
+
+
+    // console.log(arr)
+
     // if(this.state.gameTiles.status === true) {
 
 
@@ -85,8 +133,8 @@ class App extends React.Component {
           Puzzle Slider
        <button
             className="btn btn-warning"
-            onClick={() => this.renderWins()}
-          >Do stuff</button>
+            onClick={() => this.initBoard()}
+          >Init Board</button>
         </h1>
 
         <div className="row">
@@ -97,7 +145,7 @@ class App extends React.Component {
               key={index}
               className="col-3 border border-success"
               id={item.id}
-              onClick={() => this.clickAction(item.id)}
+              onClick={() => this.clickAction(item, index)}
 
             >
               {item.id}
